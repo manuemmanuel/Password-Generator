1554f4a598a30a213d3b5784906f49b8c06eb6f3
manuemmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-06-05 10:58:18 +0530

    main window

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ from tkinter import messagebox
 ctk.set_appearance_mode("dark") 
 ctk.set_default_color_theme("blue")
 character_string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
-
+password_gen = ctk.CTk()
+password_gen.geometry("400x250")
+password_gen.title("Password Generator")
