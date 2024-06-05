899ee83a8a001c332a26920cf5f039822657488b
manuemmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-06-05 10:57:34 +0530

    character string

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from tkinter import messagebox
 
 ctk.set_appearance_mode("dark") 
 ctk.set_default_color_theme("blue")
+character_string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+
