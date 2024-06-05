4daa825b1b8f75cf27a0e6a0aaab67cfa86fb628
manuemmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-06-05 10:59:45 +0530

    generate password

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ character_string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567
 password_gen = ctk.CTk()
 password_gen.geometry("400x250")
 password_gen.title("Password Generator")
+def generate_password():
+    try:
+        repeat = int(repeat_entry.get())
+        length = int(length_entry.get())
+    except ValueError:
+        messagebox.showerror(message="Please key in the required inputs")
+        return
+
+    if repeat == 1:
+        password = ''.join(random.sample(character_string, length))
+    else:
+        password = ''.join(random.choices(character_string, k=length))
+    
+    password_v.set("Created password: " + password)
